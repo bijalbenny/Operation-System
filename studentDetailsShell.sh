53643231d0b30c7e1dc8d1e9609d4ac4a61fd164
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2024-02-05 10:21:06 +0530

    studentDetailsShell.sh
    First shell propgram

---
+++ studentDetailsShell.sh
@@ -0,0 +1,11 @@
+echo "Enter your name:"
+read name
+echo "Enter your Semester"
+read semester
+echo "Enter your Batch:"
+read batch
+echo "Enter your RollNo:"
+read rollNo
+echo "Enter your RegNo:"
+read regNo
+echo -e "Name is $name\nSemester is $semester\n Batch is $batch\nRollNo is $rollNo\n RegNo is $regNo\n"
