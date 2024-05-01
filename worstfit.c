092417b1c222de4341d85529200528a2a2d20764
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2024-05-01 19:34:15 +0530

    Update worstfit.c

--- worstfit.c
+++ worstfit.c
@@ -1,4 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
+void main(){
+ int process[4],i,j;
+ struct memory{
+   int size,flag;
+ }m[4],temp;
+ printf("Enter the available memory slots\n");
+ for(i=0;i<4;i++){
+    scanf("%d",&m[i].size);
+    m[i].flag=0;
+ }
+ printf("Enter the memory used by the processes\n");
+ for(i=0;i<4;i++)
+    scanf("%d",&process[i]);
+  //swapping in descending order...
+  for(i=0;i<3;i++){
+    for(j=i+1;j<4;j++){
+        if(m[i].size<m[j].size){
+            temp = m[i];
+            m[i]=m[j];
+            m[j]=temp;
+        }
+    }
+  }
+  //ALLOCATION
+  for(i=0;i<4;i++){
+    int allocated = 0;
+    for(j=0;j<4;j++){
+        if(process[i]<=m[j].size && m[j].flag==0){
+            printf("Process %d is allocated in memory block %d\n",i+1,m[j].size);
+            m[j].flag = 1;
+            allocated = 1;
+            break;
+        }
+    }
+    if(allocated == 0)
+        printf("Process %d is not allocated\n",i+1);
+  }
+}
+
+
+
+
+
+/* 
+ #include<stdio.h>
 void main(){
 int memblok,limit,i,j;
 printf("Enter the number of memory blocks and no of process\n");
@@ -43,6 +88,7 @@ for(i=0;i<limit;i++){
      }
    }
   }
+ */
 for(i=0;i<limit;i++){
   if(p[i].flag==0)
    printf("There is no space for %d process\n",p[i].psize);
