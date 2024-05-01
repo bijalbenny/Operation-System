d62d51bc1797859349ef9eef5f016231a8d94dff
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2024-05-01 19:03:14 +0530

    Update firstfit.c

--- firstfit.c
+++ firstfit.c
@@ -43,7 +43,44 @@ void main()
        }   
    }
  
- 
+/*#include<stdio.h>
+
+int main() {
+    int process[4], i, j;
+    struct Memory {
+        int size, flag;
+    } m[4];
+
+    printf("Enter the memory used by the processes:\n");
+    for (i = 0; i < 4; i++)
+        scanf("%d", &process[i]);
+
+    printf("Enter the size of available memory slots:\n");
+    for (i = 0; i < 4; i++) {
+        scanf("%d", &m[i].size);
+        m[i].flag = 0;
+    }
+
+    // Allocation
+    for (i = 0; i < 4; i++) {
+        int allocated = 0;
+        for (j = 0; j < 4; j++) {
+            if (process[i] <= m[j].size&&m[j].flag==0) {
+
+                    printf("Process %d is allocated in memory block %d\n", i + 1, m[j].size);
+                    m[j].flag = 1;
+                    allocated = 1;
+                    break;
+
+            }
+        }
+        if (allocated == 0)
+            printf("Process %d is not allocated\n", i + 1);
+    }
+
+    return 0;
+}
+*/ 
  
  
  
