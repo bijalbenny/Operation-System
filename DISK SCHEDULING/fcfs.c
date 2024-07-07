2816a6d97e2980a31c434ce8c784adfcf16a4703
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2024-07-07 14:56:41 +0530

    Update fcfs.c

--- DISK SCHEDULING/fcfs.c
+++ DISK SCHEDULING/fcfs.c
@@ -1,5 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+   int head,i,n,distance,seektime=0,a[20];
+   printf("Enter initial head position\n");
+   scanf("%d",&head);
+   printf("Enter number of requests\n");
+   scanf("%d",&n);
+   printf("Enter requests\n");
+   for(i=1;i<=n;i++)
+    scanf("%d",&a[i]);
+   a[0]= head;
+   printf("FCFS\n");
+   for(i=0;i<n;i++){
+    distance=abs(a[i]-a[i+1]);
+    printf("Head movement from %d to %d is %d",a[i],a[i+1],distance);
+    seektime+=distance;
+   }
+   printf("Seektime is %d",seektime);
+}
+/*
 #include<stdio.h>
-
+#include <stdlib.h>
 void main() {
     int head, a[20], i, distance, n, seektime = 0;
 
@@ -25,3 +47,4 @@ void main() {
     printf("Total seek time is: %d\n", seektime);
 
 }
+*/
